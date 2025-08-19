c4f62e31fbd9f22ef9e79583e79266a348e3965d
azapien22 <azapien@hotmail.com>
2025-08-19 08:13:15 -0500

    Print Current Window

---
+++ print_contents_ofcurrent_Window/print_window.js
@@ -0,0 +1,5 @@
+// Write a JavaScript program to print the current window contents.  
+function print_current_window() {
+    // Call the window.print() method to initiate the printing of the current page
+    window.print(); 
+}
